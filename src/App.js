d06d4135a154d4e486a54cc1d2113369a5b5cea7
Chuck Kosman <cwkosman@gmail.com>
2019-07-04 20:58:00 -0700

    Add tooling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-const Pet = ({
-  name,
-  animal,
-  breed
-}) => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed)
-  ])
-}
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div", {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Boris",
-        animal: "Cat",
-        breed: "Black"
-      }),
-      React.createElement(Pet, {
-        name: "Boo Boo",
-        animal: "Dog",
-        breed: "German Shepard"
-      }),
-      React.createElement(Pet, {
-        name: "Whisker",
-        animal: "Cat",
-        breed: "Calico"
-      })
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Boris",
+      animal: "Cat",
+      breed: "Black"
+    }),
+    React.createElement(Pet, {
+      name: "Boo Boo",
+      animal: "Dog",
+      breed: "German Shepard"
+    }),
+    React.createElement(Pet, {
+      name: "Whisker",
+      animal: "Cat",
+      breed: "Calico"
+    })
+  ]);
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
