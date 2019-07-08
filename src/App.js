db1aa100bbda748daafe9a5363c11fe9519d9ba8
Chuck Kosman <cwkosman@gmail.com>
2019-07-08 12:17:45 -0700

    Async calls to API, expand pet component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <div></div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,48 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    pet.breeds("dog").then(console.log);
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedsStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedsStrings);
+    }, console.error);
+    // The second argument to useEffect is an array of dependencies
+    // Any time the value of a depedency changes, this useEffect will run again
+    // null = always run on re-render (don't do it here since test API, will infinite loop and crush API)
+    // [] = run only on the first render and never again
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -26,6 +57,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
